b99c897f3a033aae75be93fe77c9fdff55a9b4f2
Алексей Селезнёв <salex_n@inbox.ru>
2022-07-19 22:36:16 +0300

    urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -15,44 +15,20 @@ router.register(r'ingredients', IngredientsViewSet, basename='ingredients')
 
 
 urlpatterns = [
-    path('users/<int:author_id>/subscribe/',
+    path(
+        'users/<int:author_id>/subscribe/',
         FollowViewSet.as_view({'get': 'create', 'delete': 'destroy'}),
-        name='subscribe'),
-    path('users/subscriptions/', FollowViewSet.as_view({'get': 'list'}),
-         name='subscriptions'),
-    path('recipes/download_shopping_cart/',
-         DownloadShoppingCart.as_view(), name='dowload_shopping_cart'),
-
-    # path(
-    #     'recipes/<int:recipe_id>/shopping_cart/',
-    #     ShoppingCartView.as_view(),
-    #     name='shopping_cart'
-    # ),
-    # path(
-    #     'recipes/download_shopping_cart/',
-    #     download_shopping_cart,
-    #     name='download_shopping_cart'
-    # ),
-
-#     path(
-#         'users/subscriptions/',
-#         SubscribeListViewSet.as_view({'get': 'list'}),
-#         name='subscriptions'
-#     ),
-    # path(
-    #     'recipes/<int:recipe_id>/shopping_cart/',
-    #     ShoppingCartViewSet.as_view(),
-    #     name='shopping_cart'
-    # ),
-#     path(
-#         'recipes/<int:recipe_id>/favorite/',
-#         FavoriteViewSet.as_view(),
-#         name='favorite'
-#     ),
-#     path(
-#         'users/<int:user_id>/subscribe/',
-#         SubscribeView.as_view(),
-#         name='follow'
-#     ),
+        name='subscribe'
+    ),
+    path(
+        'users/subscriptions/',
+        FollowViewSet.as_view({'get': 'list'}),
+        name='subscriptions'
+    ),
+    path(
+        'recipes/download_shopping_cart/',
+        DownloadShoppingCart.as_view(),
+        name='dowload_shopping_cart'
+    ),
     path('', include(router.urls)),
 ]
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,20 +1,6 @@
 from django.urls import include, path, re_path
-# from rest_framework.routers import DefaultRouter
-
-# from .views import FollowViewSet
-
-# app_name = 'users'
-
-# router = DefaultRouter()
-# router.register(r'users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
-    # path('users/<int:author_id>/subscribe/',
-    #     FollowViewSet.as_view({'get': 'create', 'delete': 'destroy'}),
-    #     name='subscribe'),
-    # path('users/subscriptions/', FollowViewSet.as_view({'get': 'list'}),
-    #      name='subscriptions'),
-    # path('', include(router.urls)),
 ]
