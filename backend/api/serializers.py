8cbca3d56182256a5ab0c2e8c50d71f2a7119725
Алексей Селезнёв <salex_n@inbox.ru>
2022-07-29 21:30:01 +0300

    docker

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -58,7 +58,7 @@ class RecipeSerializer(serializers.ModelSerializer):
 
 class RecipeReadSerializer(serializers.ModelSerializer):
     author = UserSerializer(read_only=True)
-    ingredients = serializers.SerializerMethodField() 
+    ingredients = serializers.SerializerMethodField()
     tags = TagSerializer(many=True, read_only=True)
     is_favorited = serializers.SerializerMethodField()
     is_in_shopping_cart = serializers.SerializerMethodField()
@@ -79,7 +79,7 @@ class RecipeReadSerializer(serializers.ModelSerializer):
         )
 
     def get_ingredients(self, obj):
-        queryset = IngredientAmount.objects.filter(recipe=obj) 
+        queryset = IngredientAmount.objects.filter(recipe=obj)
         return IngredientAmountSerializer(queryset, many=True).data
 
     def get_is_favorited(self, obj):
